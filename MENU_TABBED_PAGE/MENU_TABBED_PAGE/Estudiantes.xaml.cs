90b052b75717c7de35652d1bf0b6e2739c3b0434
Misael00103 <beriguetemisael@gmail.com>
2022-08-21 21:37:56 -0400

    first commit

---
+++ MENU_TABBED_PAGE/MENU_TABBED_PAGE/Estudiantes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MENU_TABBED_PAGE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Estudiantes : ContentPage
+    {
+        public Estudiantes()
+        {
+            InitializeComponent();
+        }
+    }
+}
